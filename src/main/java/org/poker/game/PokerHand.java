20be581b417e965006dbdfcc2a12a01791de85b6
MRastogi5 <118384562+MRastogi5@users.noreply.github.com>
2023-03-11 00:43:18 +0000

    feat : Added feature to  verify input

--- src/main/java/org/poker/game/PokerHand.java
+++ src/main/java/org/poker/game/PokerHand.java
@@ -1,13 +1,23 @@
 package org.poker.game;
 
+import org.poker.game.model.Hand;
+
+import java.util.Arrays;
+
 public class PokerHand {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
-    public String checkPokerHands(Hand firstPlayer, Hand SecondPlayer){
+    public String checkPokerHands(Hand... hands){
 
+        if(hands[0].isSameHands(hands[1])){
+            return "Invalid";
+        }
+
+        if(hands[0].isSimilarHands(hands[1])){
+            return "Tie";
+        }
         return "";
-
     }
 }
--- src/test/java/org/poker/game/PokerHandTest.java
+++ src/test/java/org/poker/game/PokerHandTest.java
@@ -1,6 +1,7 @@
 package org.poker.game;
 
 import org.junit.jupiter.api.Test;
+import org.poker.game.model.Hand;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,18 +11,16 @@ public class PokerHandTest {
     @Test
     void testEmptyPokerHands(){
 
-        Hand firstPlayer = new Hand("","","","","");
-        Hand SecondPlayer = new Hand("","","","","");
+        Hand firstPlayer = new Hand(new String[]{"","","","",""});
+        Hand SecondPlayer = new Hand(new String[]{"","","","",""});
 
         assertEquals("Invalid",
                 pokerHand.checkPokerHands(firstPlayer, SecondPlayer));
     }
-
-
     @Test
     void testSimilarPokerHands(){
-        Hand firstPlayer = new Hand("2H","3D","5S","9C","KD");
-        Hand SecondPlayer = new Hand("2D","3H","5C","9S","KH");
+        Hand firstPlayer = new Hand(new String[]{"2H","3D","5S","9C","KD"});
+        Hand SecondPlayer = new Hand(new String[]{"2D","3H","5C","9S","KH"});
 
         assertEquals("Tie",
                 pokerHand.checkPokerHands(firstPlayer, SecondPlayer));
@@ -29,11 +28,20 @@ public class PokerHandTest {
 
     @Test
     void testSamePokerHands(){
-        Hand firstPlayer = new Hand("2H","3D","5S","9C","KD");
-        Hand SecondPlayer = new Hand("2H","3D","5S","9C","KD");
+        Hand firstPlayer = new Hand(new String[]{"2H","3D","5S","9C","KD"});
+        Hand SecondPlayer = new Hand(new String[]{"2H","3D","5S","9C","KD"});
 
         assertEquals("Invalid",
                 pokerHand.checkPokerHands(firstPlayer, SecondPlayer));
     }
+
+    /*@Test
+    void testSameUnOrderedPokerHands(){
+        Hand firstPlayer = new Hand( "2H","3D","5S","9C","KD");
+        Hand SecondPlayer = new Hand("5C","2D","KH","3H","9S");
+
+        assertEquals("Tie",
+                pokerHand.checkPokerHands(firstPlayer, SecondPlayer));
+    }*/
 
 }
